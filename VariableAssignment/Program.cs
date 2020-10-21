f8d8e8ba0d59618f916a5b17b34669a68f1c677f
Bruno Costa <brunoarruda697@gmail.com>
2020-10-21 10:55:36 -0300

    Atribuição de valor para as variáveis

---
+++ VariableAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VariableAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberValue = 10;
+            Console.WriteLine(numberValue);
+
+            numberValue += 2;
+            Console.WriteLine(numberValue);
+
+            numberValue *= 2;
+            Console.WriteLine(numberValue);
+            Console.WriteLine("==========================");
+            numberValue++;
+            Console.WriteLine(numberValue);
+
+            numberValue--;
+            Console.WriteLine(numberValue);
+            Console.WriteLine("==========================");
+            string stringValue = "ABC";
+            Console.WriteLine(stringValue);
+
+            stringValue += "DEF";
+            Console.WriteLine(stringValue);
+        }
+    }
+}
